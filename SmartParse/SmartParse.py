5e3c12eca5611f2ed6125b0a41b729578d789c3d
Harrison Sherwood <harharharry123@gmail.com>
2024-12-19 13:08:12 -0800

    removed init

--- SmartParse/SmartParse.py
+++ SmartParse/SmartParse.py
@@ -3,15 +3,6 @@ from .get_languages import GetLanguages
 import bisect
 
 class CodeParser:
-    def __init__(self, language_name, file_path, file_content):
-        self.language = Language(GetLanguages.get_language(language_name))
-        self.parser = Parser(self.language)
-        self.file_path = file_path
-        self.file_content = file_content
-        self.tree = self.parser.parse(self.file_content, encoding="utf8")
-        self.lines = self.split_lines(self.file_content)
-        self.lines_depth = {}
-        self.results = self.make_chunks()
 
     def split_lines(self, file_content):
         lines = []
@@ -95,31 +86,12 @@ class CodeParser:
         
         return results
             
-    def make_chunks(self):
-        results = self.walk(self.tree.root_node, self.lines, self.lines_depth)
+    def make_chunks(self, language_name, file_content):
+        language = Language(GetLanguages.get_language(language_name))
+        parser = Parser(language)
+        tree = parser.parse(file_content, encoding="utf8")
+        lines = self.split_lines(file_content)
+        lines_depth = {}
+        results = self.walk(tree.root_node, lines, lines_depth)
         chunks = self.combine(results)
         return chunks
-
-    def print_chunks(self):
-        for chunk in self.results.items():
-            print(f"\033[2m**NODE: {chunk[0]}**\033[0m\n")
-            for line in chunk[1]:
-                if line[0] == 'n':
-                    print(f"\033[90mnode {line[1]}\033[0m")
-                else:
-                    print(f"\033[90mline {line[1]}\033[0m")
-            print("\n" + ("-" * 100))
-
-    def find_lines(self, selected_lines):
-        selected_set = set(selected_lines)
-        for chunk in self.results.items():
-            common_lines = selected_set.intersection(chunk[1])
-            if common_lines:
-                print(f"\033[2m**NODE: {chunk[0]}**\033[0m\n")
-                print(chunk[1][0][0].decode("utf-8"))
-                for line in common_lines:
-                    if line != chunk[1][0] and line != chunk[1][-1]:
-                        print(line[0].decode("utf-8"))
-                if chunk[1][-1][0] != 'n':
-                    print(chunk[1][-1][0].decode("utf-8"))
-                print("\n" + ("-" * 100))
