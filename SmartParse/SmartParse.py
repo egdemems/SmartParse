d1f8db2a136e7c7939ce43b30f7d1e3b5a7f0e17
Harrison Sherwood <harharharry123@gmail.com>
2024-12-21 02:00:27 -0800

    simple named nodes walk

--- SmartParse/SmartParse.py
+++ SmartParse/SmartParse.py
@@ -2,44 +2,32 @@ from tree_sitter import Language, Parser
 from .get_languages import GetLanguages
 
 class CodeParser:
-    
-    def split_lines(self, file_content):
-        lines = []
-        start_byte = 0
-        for line in file_content.splitlines(keepends=True):
-            end_byte = start_byte + len(line) - 1
-            lines.append((line, start_byte, end_byte))
-            start_byte = end_byte + 1
-        return lines
-
-    def walk(self, min_lines, file_content, parent, lines, nodes={}, seen_lines=None):
+  
+    def walk(self, parent, lines, nodes={}, seen_lines=[]):
         if seen_lines is None:
             seen_lines = []
-        for child in parent.children:
+        enumerated_lines = list(enumerate(lines))
+        for child in parent.named_children:
             identifier = child.type
-            start = parent.start_byte
-            end = child.end_byte
-            
-            self.walk(min_lines, file_content, child, lines, nodes, seen_lines)
-            relevant_lines = [
-                (index, line)
-                for index, line in enumerate(lines)
-                if line[1] >= start and line [2] <= end and (index, line) not in seen_lines
-            ]
-            
-            if len(relevant_lines) >= min_lines:
-                seen_lines.extend(relevant_lines)
-                nodes[start] = relevant_lines
+            start_line = child.start_point[0]
+            end_line = child.end_point[0] + 1
+            self.walk(child, lines, nodes)
+            if len(child.text.splitlines()) > 1 and child.named_child_count > 1:
+                for line in enumerated_lines[start_line:end_line]:
+                    if line not in seen_lines:
+                        seen_lines.append(line)
+                        if child.start_byte not in nodes:
+                            nodes[child.start_byte] = [line]
+                        else:
+                            nodes[child.start_byte].append(line)
             
         return nodes
     
-    def make_chunks(self, min_lines, language_name, file_content):
+    def make_chunks(self, language_name, file_content):
         language = Language(GetLanguages.get_language(language_name))
         parser = Parser(language)
         tree = parser.parse(file_content, encoding="utf8")
-        lines = self.split_lines(file_content)
-        results = self.walk(min_lines, file_content, tree.root_node, lines)
+        lines = file_content.splitlines()
+        print(len(lines))
+        results = self.walk(tree.root_node, lines)
         return results
-
-
-
