02061e62f48e0d119fa6e58cbf6193e373a11723
Harrison Sherwood <harharharry123@gmail.com>
2024-12-26 16:18:49 -0800

    improved chunking

--- SmartParse/SmartParse.py
+++ SmartParse/SmartParse.py
@@ -3,23 +3,22 @@ from .get_languages import GetLanguages
 
 class CodeParser:
   
-    def walk(self, parent, lines, seen_lines, nodes=None):
+    def walk(self, parent, lines, seen_lines, count, nodes=None):
         if nodes is None:
             nodes = {}
         for child in parent.named_children:
             identifier = child.type
             start = child.start_point[0]
             end = child.end_point[0] + 1
-            self.walk(child, lines, seen_lines, nodes)
+            self.walk(child, lines, seen_lines, count, nodes)
             child_lines = [line for line in lines[start:end] if line not in seen_lines]
-            if len(child_lines) > 4 and child.named_child_count > 1:
-                for line in child_lines:
-                    if line not in seen_lines:
-                        seen_lines.append(line)
-                        if start not in nodes:
-                            nodes[start] = [line]
-                        else:
-                            nodes[start].append(line)
+            if len(child_lines) > count:
+                if len(child_lines) < 50:
+                    nodes[start] = child_lines
+                    seen_lines.extend(child_lines)
+                else:
+                    print("hi")
+                    self.walk(child, lines, seen_lines, count - 1, nodes)
             
         return nodes
     
@@ -34,8 +33,8 @@ class CodeParser:
             chunk_pairs.append(chunk_pair)
         return chunk_pairs
 
-    def get_chunks(self, language, file_content, line_numbers=True):
-        chunks = self.make_chunks(language=language, file_content=file_content)
+    def get_chunks(self, language, file_content, count, line_numbers=True):
+        chunks = self.make_chunks(language=language, file_content=file_content, count=count)
         text_chunks = self.get_text_chunks(chunks, line_numbers)
         chunk_pairs = self.get_chunk_pairs(chunks, text_chunks)
         return chunk_pairs
@@ -59,13 +58,15 @@ class CodeParser:
             text_chunks.append(chunk_text)
         return text_chunks
     
-    def make_chunks(self, language, file_content):
+    def make_chunks(self, language, file_content, count):
         language = Language(GetLanguages.get_language(language))
         parser = Parser(language)
         tree = parser.parse(file_content, encoding="utf8")
         lines = self.get_lines(file_content)
         seen_lines=[]
-        results = self.walk(tree.root_node, lines, seen_lines)
+        results = self.walk(tree.root_node, lines, seen_lines, count)
         extra = [line for line in lines if line not in seen_lines]
-        results[extra[0][0]] = extra
+        if extra:
+            if extra[0][0] in results:
+                results[extra[0][0]] = extra
         return results
